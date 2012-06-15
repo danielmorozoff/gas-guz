641d14b5769d4f3c7cf31ae8e613698efece2780
Daniel Morozoff <danielmorozoff@gmail.com>
2012-06-15 14:20:18 -0700

    Removed extra dir

---
+++ src/com/gasguz/action/admin/LogonAction.java
@@ -0,0 +1,31 @@
+package com.gasguz.action.admin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+public class LogonAction extends AbstractAdminAction {
+  private static Log log = LogFactory.getLog(LogonAction.class);
+
+  
+  public ActionForward unspecified(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    return home(mapping, form, request, response);
+  }
+
+  
+  public ActionForward home(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    DynaActionForm oForm = (DynaActionForm)form;
+    
+    log.debug("logon attempt from " + oForm.getString("username"));
+
+    return mapping.findForward("home");
+  }
+
+
+}
